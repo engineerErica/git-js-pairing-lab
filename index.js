4a29ab48f3d6c1205639ce64c8866706ecacc23a
gonzzoh <gmeza0118@gmail.com>
2023-03-29 17:51:42 +0000

    problem 2 finished

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 //Code your solutions in this file
+// Problem #1
 function fiveToOneHundred(){
     for (let i = 5; i <= 100; i++){
         console.log(i)
     }
 }
 fiveToOneHundred()
+
+// Problem #2
+function multiplesOfThree() {
+    for (let i = 3; i < 100; i+=3) {
+        console.log(i)
+    }
+}
+multiplesOfThree()
